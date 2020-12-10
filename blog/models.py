a21e108178fbba07a5a6b275b11bf3e27df5ba52
Tony35 <tonyharris3@msn.com>
2020-12-10 08:55:06 -0500

    Added bootstrap and jobs to home page view

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+# create a Blog models
+    # title
+    # pub_date
+    # body
+    # image
+class Blog(models.Model):
+    image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+   
+# Add the Blog app to settings
+# create a makemigration
+# migrate
+# add to the admin
