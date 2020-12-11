b097e0ecb1ee75b89ed4894044edba5583a78784
Tony35 <tonyharris3@msn.com>
2020-12-11 14:28:35 -0500

    Blog fuctionality and polish

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,15 @@ class Blog(models.Model):
     title = models.CharField(max_length=255)
     pub_date = models.DateTimeField()
     body = models.TextField()
+
+    def __str__(self):
+        return self.title
+    
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
    
 # Add the Blog app to settings
 # create a makemigration
